0f595bd2e63a59975e244398ca71d347a0e90aaf
Joaquin Cibanal <joaquin@MacBook-Pro-de-Joaquin.local>
2023-04-18 18:07:38 -0300

    change to read files from a .csv to a MySQL database

--- src/main/java/Project/Logica.java
+++ src/main/java/Project/Logica.java
@@ -1,9 +1,10 @@
 package Project;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,47 +13,49 @@ import java.util.Map;
 public class Logica {
 
     public static void main(String[] args) {
-        
-        try {
-            
-            File resultados = new File("/Users/joacib/eclipse-workspace/ap-project/src/main/java/resultados.csv");
-            File pronosticos = new File("/Users/joacib/eclipse-workspace/ap-project/src/main/java/pronosticos.csv");
-            
-            String line = "";
-            String cvsSplitBy = ";";
-            
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", "");
+            Statement stmt = con.createStatement();
+
+            // Seleccionar todos los resultados de la tabla "resultados"
+            ResultSet rsResultados = stmt.executeQuery("SELECT * FROM resultados");
+
             List<Equipo> equipos = new ArrayList<Equipo>();
             List<Partido> partidos = new ArrayList<Partido>();
-            Map<String, List<Pronostico>> pronosticosPorPersona = new HashMap<String, List<Pronostico>>();
-            
-            BufferedReader brResultados = new BufferedReader(new FileReader(resultados));
-            while ((line = brResultados.readLine()) != null) {
-                String[] datos = line.split(cvsSplitBy);
-                String ronda = datos[0];
-                String eq1 = datos[1];
-                int golesEq1 = Integer.parseInt(datos[2]);
-                int golesEq2 = Integer.parseInt(datos[3]);
-                String eq2 = datos[4];
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Cuantos puntos quiere que de acertar el resultado?");
+            int puntaje = scanner.nextInt();
+
+            while (rsResultados.next()) {
+                String ronda = rsResultados.getString("ronda");
+                String eq1 = rsResultados.getString("eq1");
+                int golesEq1 = rsResultados.getInt("golesEq1");
+                int golesEq2 = rsResultados.getInt("golesEq2");
+                String eq2 = rsResultados.getString("eq2");
 
                 Equipo equi1 = new Equipo(eq1,"");
                 Equipo equi2 = new Equipo(eq2,"");
                 equipos.add(equi1);
                 equipos.add(equi2);
-                
+
                 Partido partido = new Partido(equi1,equi2,golesEq1,golesEq2);
                 partidos.add(partido);
             }
-            brResultados.close();
-            
-            BufferedReader brPronosticos = new BufferedReader(new FileReader(pronosticos));
-            while ((line = brPronosticos.readLine()) != null) {
-                String[] datos = line.split(cvsSplitBy);
-                String nombre = datos[0];
-                String eq1 = datos[1];
-                String eq2 = datos[5];
-                boolean gana = datos[2].equals("X");
-                boolean empata = datos[3].equals("X");
-                boolean pierde = datos[4].equals("X");
+
+            // Seleccionar todos los pronósticos de la tabla "pronosticos"
+            ResultSet rsPronosticos = stmt.executeQuery("SELECT * FROM pronosticos");
+
+            Map<String, List<Pronostico>> pronosticosPorPersona = new HashMap<String, List<Pronostico>>();
+
+            while (rsPronosticos.next()) {
+                String nombre = rsPronosticos.getString("nombre");
+                String eq1 = rsPronosticos.getString("eq1");
+                String eq2 = rsPronosticos.getString("eq2");
+                boolean gana = rsPronosticos.getString("gana")!=null;
+                boolean empata = rsPronosticos.getString("empata")!=null;
+                boolean pierde = rsPronosticos.getString("pierde")!=null;
 
                 String resultado = "";
                 if (gana) {
@@ -64,52 +67,49 @@ public class Logica {
                         resultado = "pierde";
                     }
                 }
-                
+
                 Pronostico pronostico = new Pronostico(eq1, resultado, eq2);
                 if (!pronosticosPorPersona.containsKey(nombre)) {
                     pronosticosPorPersona.put(nombre, new ArrayList<Pronostico>());
                 }
                 pronosticosPorPersona.get(nombre).add(pronostico);
             }
-            brPronosticos.close();
-            
-            Map<String, Integer> puntosPorPersona = new HashMap<String, Integer>();
-            
+
             // Calcular puntos por persona
+            Map<String, Integer> puntosPorPersona = new HashMap<String, Integer>();
+
             for (String nombre : pronosticosPorPersona.keySet()) {
                 int puntos = 0;
                 List<Pronostico> pronosticos1 = pronosticosPorPersona.get(nombre);
                 for (int i = 0; i < pronosticos1.size(); i++) {
                     Pronostico pronostico = pronosticos1.get(i);
                     for (int j = 0; j < partidos.size(); j++) {
-                        Partido partido = partidos.get(j);
-                        if (pronostico.getEquipoLocal().equals(partido.getEquipoLocal().getNombre()) &&
-                                pronostico.getEquipoVisitante().equals(partido.getEquipoVisitante().getNombre())) {
-                            if (pronostico.getResultado().equals(partido.getResultado())) {
-                                puntos += 1; // Modificación aquí
-                            } else if (pronostico.getResultado().equals("gana") && partido.getGolesEquipoLocal() > partido.getGolesEquipoVisitante()) {
-                                puntos += 1;
-                            } else if (pronostico.getResultado().equals("empata") && partido.getGolesEquipoLocal() == partido.getGolesEquipoVisitante()) {
-                                puntos += 1;
-                            } else if (pronostico.getResultado().equals("pierde") && partido.getGolesEquipoVisitante() > partido.getGolesEquipoLocal()) {
-                                puntos += 1;
-                            }
-                        }
-                    }
-                }
-                puntosPorPersona.put(nombre, puntos);
-            }
 
-            // Imprimir tabla de posiciones
-            System.out.println("Tabla de posiciones:");
-            System.out.println("Nombre\tPuntos");
-            for (String nombre : puntosPorPersona.keySet()) {
-                System.out.println(nombre + "\t" + puntosPorPersona.get(nombre));
-            }
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    	                Partido partido = partidos.get(j);
+    	                if (pronostico.getEquipoLocal().equals(partido.getEquipoLocal().getNombre()) &&
+    	                        pronostico.getEquipoVisitante().equals(partido.getEquipoVisitante().getNombre())) {
+    	                    if (pronostico.getResultado().equals(partido.getResultado())) {
+    	                        puntos += 1; // Modificación aquí
+    	                    } else if (pronostico.getResultado().equals("gana") && partido.getGolesEquipoLocal() > partido.getGolesEquipoVisitante()) {
+    	                        puntos += 1;
+    	                    } else if (pronostico.getResultado().equals("empata") && partido.getGolesEquipoLocal() == partido.getGolesEquipoVisitante()) {
+    	                        puntos += 1;
+    	                    } else if (pronostico.getResultado().equals("pierde") && partido.getGolesEquipoVisitante() > partido.getGolesEquipoLocal()) {
+    	                        puntos += 1;
+    	                    }
+    	                }
+    	            }
+    	        }
+    	        puntosPorPersona.put(nombre, puntos);
+    	    }
+
+    	    // Imprimir tabla de posiciones
+    	    System.out.println("Tabla de posiciones:");
+    	    System.out.println("Nombre\tPuntos");
+    	    for (String nombre : puntosPorPersona.keySet()) {
+    	        System.out.println(nombre + "\t" + puntosPorPersona.get(nombre));
+    	    }
+    	    
+		 } catch(Exception e){ System.out.println(e);}
+        }	
     }
-
-}
